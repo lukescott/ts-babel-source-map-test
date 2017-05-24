4de7bf50d17636254ca8a2eb54bcba7a5220f4f9
Luke Scott <luke@webconnex.com>
2017-05-23 23:57:16 -0700

    Add transformed files for preprocessor1

---
+++ preprocessor1/orig->ts.js
@@ -0,0 +1,10 @@
+import bar from "../bar/bar";
+export default bar({
+    input(state, value = "") {
+        if (!value) {
+            return null;
+        }
+        return state;
+    },
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZm9vLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiZm9vLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLE9BQU8sR0FBRyxNQUFNLFlBQVksQ0FBQTtBQUU1QixlQUFlLEdBQUcsQ0FBQztJQUNsQixLQUFLLENBQUMsS0FBSyxFQUFFLEtBQUssR0FBRyxFQUFFO1FBQ3RCLEVBQUUsQ0FBQyxDQUFDLENBQUMsS0FBSyxDQUFDLENBQUMsQ0FBQztZQUNaLE1BQU0sQ0FBQyxJQUFJLENBQUE7UUFDWixDQUFDO1FBQ0QsTUFBTSxDQUFDLEtBQUssQ0FBQTtJQUNiLENBQUM7Q0FDRCxDQUFDLENBQUEifQ==
